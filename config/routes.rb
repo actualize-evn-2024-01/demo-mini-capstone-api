78be7d6c7d874cc9acd224f0e958f69b947ca0a8
Peter Jang <peter.x.jang@gmail.com>
2024-02-26 20:49:25 -0600

    Change one product routes to a single variable route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "/all_products", controller: "products", action: "all_products"
-  get "/first_product", controller: "products", action: "first_product"
-  get "/second_product", controller: "products", action: "second_product"
+  get "/one_product/:id", controller: "products", action: "one_product"
 end
