4b5142c67276854f942b46caf6e2ac55e36fea76
Peter Jang <peter.x.jang@gmail.com>
2024-02-29 19:25:11 -0600

    Add model methods fo product money stuff

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,21 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    result = true
+    if price <= 10
+      result = true
+    else
+      result = false
+    end
+    result
+  end
+
+  def tax
+    result = price * 0.09
+    result
+  end
+
+  def total
+    result = price * 1.09
+    result
+  end
 end
