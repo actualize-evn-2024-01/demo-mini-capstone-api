b40fc6e8684b0609cd44437a65215aaf7d8bc982
Peter Jang <peter.x.jang@gmail.com>
2024-03-18 20:32:13 -0500

    Fix show test with new json

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -14,7 +14,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response 200
 
     data = JSON.parse(response.body)
-    assert_equal ["id", "name", "price", "description", "created_at", "updated_at", "is_discounted?", "tax", "total", "supplier", "images"], data.keys
+    assert_equal ["id", "name", "price", "description", "created_at", "updated_at", "is_discounted?", "tax", "total", "supplier", "images", "primary_image"], data.keys
   end
 
   test "create" do
