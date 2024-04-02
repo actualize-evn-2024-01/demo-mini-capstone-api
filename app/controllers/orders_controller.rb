e0912417d1c120b416e68b2e88f75e880e78abaa
Peter Jang <peter.x.jang@gmail.com>
2024-04-01 20:08:16 -0500

    Refactor business logic into model method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,26 +8,12 @@ class OrdersController < ApplicationController
   end
 
   def create
-    carted_products = current_user.carted_products.where(status: "carted")
-    calculated_subtotal = 0
-    calculated_tax = 0
-    carted_products.each do |carted_product|
-      calculated_subtotal += carted_product.quantity * carted_product.product.price
-      calculated_tax += carted_product.quantity * carted_product.product.tax
-    end
-    calculated_total = calculated_subtotal + calculated_tax
-
     @order = Order.create(
       user_id: current_user.id,
-      subtotal: calculated_subtotal,
-      tax: calculated_tax,
-      total: calculated_total,
     )
-
-    carted_products.each do |carted_product|
-      carted_product.update(status: "purchased", order_id: @order.id)
-    end
-
+    carted_products = current_user.carted_products.where(status: "carted")
+    carted_products.update_all(status: "purchased", order_id: @order.id)
+    @order.update_totals
     render :show
   end
 
